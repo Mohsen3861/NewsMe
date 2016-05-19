c94e0440f4532d2a74f260240810f27567394d84
Mohsen3861 <mohsenraic@hotmail.fr>
2016-05-20 00:02:40 +0200

    -liste d'articles

--- app/src/main/java/com/esgi/newsme/newsme/Activities/MainActivity.java
+++ app/src/main/java/com/esgi/newsme/newsme/Activities/MainActivity.java
@@ -1,15 +1,54 @@
 package com.esgi.newsme.newsme.Activities;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.esgi.newsme.newsme.Adapters.ArticleAdapter;
+import com.esgi.newsme.newsme.Models.Article;
 import com.esgi.newsme.newsme.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView mainNewsList ;
+    ArticleAdapter articleAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        assignViews();
+        prepareList();
+    }
+
+    public void assignViews(){
+        mainNewsList = (ListView) findViewById(R.id.listViewMain);
+    }
+
+    public void prepareList(){
+        articleAdapter = new ArticleAdapter(this);
+      //  ArrayList<Article> newsList = new ArrayList<>();
+
+        Article article = new Article();
+        article.setTitle("L’Ile-de-France adopte le principe des tests salivaires de détection de drogue dans les lycées\n");
+        article.setSource("lemonde.fr");
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.image_test);
+        article.setImage(largeIcon);
+
+        Article article2 = new Article();
+        article2.setTitle("Conflit israélo-palestinien : la conférence de Paris aura lieu le 3 juin, en présence de Kerry\n");
+        article2.setSource("lemonde.fr");
+        Bitmap largeIcon2 = BitmapFactory.decodeResource(getResources(), R.drawable.image_test3);
+        article2.setImage(largeIcon2);
+
+        articleAdapter.addItem(article);
+        articleAdapter.addItem(article2);
+
+        mainNewsList.setAdapter(articleAdapter);
+
+
     }
 }
---
+++ app/src/main/java/com/esgi/newsme/newsme/Adapters/ArticleAdapter.java
@@ -0,0 +1,91 @@
+package com.esgi.newsme.newsme.Adapters;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.esgi.newsme.newsme.Models.Article;
+import com.esgi.newsme.newsme.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mohsen raeisi on 19/05/2016.
+ */
+public class ArticleAdapter extends BaseAdapter{
+
+    private List<Article> mData = new ArrayList<>();
+    private LayoutInflater mInflater;
+
+    public ArticleAdapter (Context context){
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    public void addItem( Article item) {
+        mData.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void addItemsCollection(List<Article> elements){
+        mData.clear();
+        mData.addAll(elements);
+        notifyDataSetChanged();
+
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ArticleViewHolder holder = null;
+        if( convertView == null){
+            convertView = mInflater.inflate(R.layout.article_cell,null);
+
+            holder = new ArticleViewHolder();
+            holder.title = (TextView) convertView.findViewById(R.id.textView_title);
+            holder.source = (TextView) convertView.findViewById(R.id.textView_source);
+            holder.image = (ImageView) convertView.findViewById(R.id.imageView_article);
+            holder.shareButton = (ImageView) convertView.findViewById(R.id.imageView_share);
+            holder.shareButton = (ImageView) convertView.findViewById(R.id.imageView_favorit);
+
+
+
+            convertView.setTag(holder);
+        }else{
+            holder = (ArticleViewHolder) convertView.getTag();
+        }
+        holder.title.setText(mData.get(position).getTitle());
+        holder.source.setText(mData.get(position).getSource());
+        holder.image.setImageBitmap(mData.get(position).getImage());
+
+        return convertView;
+    }
+
+    static class ArticleViewHolder {
+        public TextView title;
+        public TextView source;
+        public ImageView image;
+        public ImageView favoritButton;
+        public ImageView shareButton;
+    }
+}
