a3068e9a9ac0e0760a66c611864db987f55f020c
Mohsen3861 <mohsenraic@hotmail.fr>
2016-07-01 16:25:03 +0200

    -model user

---
+++ app/src/main/java/com/esgi/newsme/newsme/Models/User.java
@@ -0,0 +1,53 @@
+package com.esgi.newsme.newsme.Models;
+
+/**
+ * Created by mohsen raeisi on 01/07/2016.
+ */
+public class User {
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String mdp;
+
+    public  User(){}
+
+    public User(String nom, String prenom, String email, String mdp) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.mdp = mdp;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+}
